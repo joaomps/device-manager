581f699d69c879eafa6fcda093c0be29866e5a31
João Silva <joaomps18@gmail.com>
2025-03-16 02:00:38 +0000

    add no-args constructor to Device class

--- src/main/java/com/joaomps/devicemanager/model/Device.java
+++ src/main/java/com/joaomps/devicemanager/model/Device.java
@@ -11,11 +11,13 @@ import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "model_devices")
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class Device {
