e5407c989a93927cd570af43ba944433b8be67ad
João Silva <joaomps18@gmail.com>
2025-03-16 01:49:51 +0000

    add DeviceController for device creation endpoint

---
+++ src/main/java/com/joaomps/devicemanager/controller/DeviceController.java
@@ -0,0 +1,27 @@
+package com.joaomps.devicemanager.controller;
+
+import com.joaomps.devicemanager.model.Device;
+import com.joaomps.devicemanager.service.DeviceService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/devices")
+public class DeviceController {
+
+  private final DeviceService deviceService;
+
+  public DeviceController(DeviceService deviceService) {
+    this.deviceService = deviceService;
+  }
+
+  @PostMapping
+  public ResponseEntity<Device> createDevice(@Valid @RequestBody Device device) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(deviceService.createDevice(device));
+  }
+}
