23cf19fbd8078cbad34903415478d3a18c53c07a
João Silva <joaomps18@gmail.com>
2025-03-16 12:46:51 +0000

    add custom exception classes for device management

---
+++ src/main/java/com/joaomps/devicemanager/exception/InvalidOperationException.java
@@ -0,0 +1,8 @@
+package com.joaomps.devicemanager.exception;
+
+public class InvalidOperationException extends RuntimeException {
+
+  public InvalidOperationException(String message) {
+    super(message);
+  }
+}
