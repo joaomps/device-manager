f5a35bd5866e83a1599b224dbd59848f10a594d7
João Silva <joaomps18@gmail.com>
2025-03-16 00:56:05 +0000

    initialize project structure with Spring Boot, Docker Compose, and Maven wrapper

---
+++ src/test/java/com/joaomps/device_manager/DeviceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaomps.device_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
