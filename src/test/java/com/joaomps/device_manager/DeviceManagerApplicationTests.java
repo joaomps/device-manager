32f9927affb91d84cc3befe1075f33bd4414f319
João Silva <joaomps18@gmail.com>
2025-03-16 01:32:43 +0000

    add H2 database dependency and configure test database settings

--- src/test/java/com/joaomps/device_manager/DeviceManagerApplicationTests.java
+++ src/test/java/com/joaomps/device_manager/DeviceManagerApplicationTests.java
@@ -1,9 +1,20 @@
 package com.joaomps.device_manager;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.MOCK)
+@TestPropertySource(properties = {
+		"spring.jpa.hibernate.ddl-auto=none",
+		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.username=sa",
+		"spring.datasource.password=",
+		"spring.datasource.driver-class-name=org.h2.Driver"
+})
+@AutoConfigureTestDatabase
 class DeviceManagerApplicationTests {
 
 	@Test
