2b4d103cc375735c14e878629894e961a98b2d3d
João Silva <joaomps18@gmail.com>
2025-03-16 01:39:30 +0000

    add Device and DeviceState classes for device management

---
+++ src/main/java/com/joaomps/devicemanager/model/Device.java
@@ -0,0 +1,35 @@
+package com.joaomps.devicemanager.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "model_devices")
+@AllArgsConstructor
+@Getter
+@Setter
+public class Device {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+  private String brand;
+
+  @Enumerated(EnumType.STRING)
+  private DeviceState state;
+
+  @Column(updatable = false)
+  private LocalDateTime creationTime;
+}
---
+++ src/main/java/com/joaomps/devicemanager/model/DeviceState.java
@@ -0,0 +1,5 @@
+package com.joaomps.devicemanager.model;
+
+public enum DeviceState {
+  AVAILABLE, IN_USE, INACTIVE
+}
